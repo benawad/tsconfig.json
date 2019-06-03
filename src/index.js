44c12b17e2a37c6cb21a41882b4c9200a41ff23d
Ben Awad <benawad97@gmail.com>
2019-06-02 19:57:05 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const inquirer = require("inquirer");
+const { writeFileSync } = require("fs");
+
+const tsconfigs = {
+  react: JSON.stringify(require("./config/tsconfig.react.json"), null, 2),
+  "react-native": JSON.stringify(
+    require("./config/tsconfig.react-native.json"),
+    null,
+    2
+  ),
+  node: JSON.stringify(require("./config/tsconfig.react-native.json"), null, 2)
+};
+
+inquirer
+  .prompt([
+    {
+      type: "list",
+      message: "Pick the framework your using:",
+      name: "framework",
+      choices: ["react", "react-native", "node"]
+    }
+  ])
+  .then(({ framework }) => {
+    const tsconfigToWrite = tsconfigs[framework];
+
+    const cwd = process.cwd();
+    writeFileSync(cwd + "/tsconfig.json", tsconfigToWrite);
+    console.log("tsconfig.json successfully created");
+  });
