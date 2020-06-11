81036710d4c1df54cfd4b9165b4de62223c44ec4
HarshitJoshi9152 <harsjoshi1614@gmail.com>
2020-06-11 23:54:06 +0530

    made adding new config files easier, hard coding config file names no longer required

--- src/index.js
+++ src/index.js
@@ -1,29 +1,48 @@
 #!/usr/bin/env node
 const inquirer = require("inquirer");
-const { writeFileSync } = require("fs");
-const tsconfigNode = require("./config/tsconfig.node.json");
-const tsconfigReact = require("./config/tsconfig.react.json");
-const tsconfigReactNative = require("./config/tsconfig.react-native.json");
+const { writeFile, readdir, readFile } = require("fs").promises;
 
-const tsconfigs = {
-  "node": tsconfigNode,
-  "react": tsconfigReact,
-  "react-native": tsconfigReactNative,
-};
+const configFiles = {};
+const configFolderPath = __dirname + '\\config'; 
 
 (async () => {
+
+  const files = await readdir(configFolderPath).catch(console.log);
+
+  for (let i of files) {
+    // framework name is situated between 2 dots eg- react between 2 '.'(s)
+    const [start, end] = getAllRegexIndexes(i, /\./g);
+    const prop = i.slice(start + 1, end);
+    configFiles[prop] = configFolderPath + '/' + i;
+  }
+
   const { framework } = await inquirer.prompt([
     {
       type: "list",
       message: "Pick the framework you're using:",
       name: "framework",
-      choices: ["react", "react-native", "node"]
+      choices: Object.keys(configFiles),
     }
   ]);
 
+  const config = await readFile(configFiles[framework]).catch(console.log);
+
   const cwd = process.cwd();
 
-  writeFileSync(`${cwd}/tsconfig.json`, JSON.stringify(tsconfigs[framework], null, 2));
-
+  await writeFile(`${cwd}/tsconfig.json`, JSON.stringify(config.toString(), null, 2));
+ 
   console.log("tsconfig.json successfully created");
 })()
+
+
+
+// should we just call /\./g twice ?
+function getAllRegexIndexes(str, regexp) {
+  let container = [];
+  while(container[container.length - 1] !== null) {
+    let index = regexp.exec(str); // .exec returns null when no match is found to we have to watch out for it to avoid error without executing .exec twice
+    index = (index) ? index.index : null ;
+    container.push(index);
+  }
+  return container.slice(0, container.length - 1);
+}
