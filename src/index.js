2ad34606c9133a115f5f64c7dad76ac083c1dbb4
Divyanshu Parihar <58904683+divyanshuParihar265@users.noreply.github.com>
2021-07-19 18:13:03 +0530

    added better message description
    As Node is also an option , better message should use "Technology " keyword rather than "framework"

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const configFolderPath = path.resolve(__dirname, 'config');
   const { framework } = await inquirer.prompt([
     {
       type: "list",
-      message: "Pick the framework you're using:",
+      message: "Pick the technology you're using:",
       name: "framework",
       choices: Object.keys(configFiles),
     }
